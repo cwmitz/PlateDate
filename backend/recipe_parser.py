a74155799dfed294411ec6367a1164b4789d8dc4
jackie-wells <jrw363@cornell.edu>
2024-03-21 14:50:45 -0400

    Parse ingredients function
    file name changes and parse ingredients

--- backend/recipe_parser.py
+++ backend/recipe_parser.py
@@ -9,15 +9,15 @@ def dietary_restrictions_check(recipe):
 
 def parse_ingredients(recipe):
     """
-    TODO: Add docstring.
+    Takes in the recipe and returns a list of the ingredients.
     """
-    raise NotImplementedError
-    # ingredients = recipe["RecipeIngredientParts"]
-
+    ingredients = recipe["RecipeIngredientParts"]
+    ingredients = re.findall(r'"(.*?)"', ingredients)
+    return ingredients
 
 def parse_instructions(recipe):
     """
-    Takes in the recipe and returns a list of the steps in the recipe.
+    Takes in the recipe and returns a list of the instruction steps.
     """
     instructions = recipe["RecipeInstructions"]
     steps = re.findall(r'"(.*?)"', instructions)
