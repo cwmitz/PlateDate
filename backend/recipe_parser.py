3c2430716dd5aa8736b6d43141d63e9652e954d3
Christina Unkenholz <ccu6@cornell.edu>
2024-03-21 20:23:44 -0400

    Remade jsons so that instructions were in paragraph format

--- backend/recipe_parser.py
+++ backend/recipe_parser.py
@@ -1,5 +1,6 @@
 import re
 
+
 def dietary_restrictions_check(recipe):
     """
     TODO: Add docstring.
@@ -15,10 +16,14 @@ def parse_ingredients(recipe):
     ingredients = re.findall(r'"(.*?)"', ingredients)
     return ingredients
 
+
 def parse_instructions(recipe):
     """
-    Takes in the recipe and returns a list of the instruction steps.
+    Takes in the recipe and returns a string of the instruction steps with a space after each period.
     """
     instructions = recipe["RecipeInstructions"]
     steps = re.findall(r'"(.*?)"', instructions)
-    return steps
+    i = ""
+    for step in steps:
+        i += step + ' '
+    return i
