0fc6cd7b035d50fc0338f00e8ca41ee65924704b
jackie-wells <jrw363@cornell.edu>
2024-04-14 12:12:47 -0400

    Dietary Restrictions check function in recipe_parser
    - Completed function with preliminary basic check for dietary restrictions keywords

--- backend/recipe_parser.py
+++ backend/recipe_parser.py
@@ -3,10 +3,16 @@ import re
 
 def dietary_restrictions_check(recipe):
     """
-    TODO: Add docstring.
+    Takes in the recipe and returns a list of booleans correcponding to 
+    [gluten free, dairy free, vegetarian, vegan, and nut free] 
     """
-    raise NotImplementedError
-
+    keywords = re.findall(r'"([^"]*)"', recipe["Keywords"].lower())
+    vegan = "vegan" in keywords
+    vegetarian = ("vegetarian" in keywords) or vegan
+    gluten_free = ("gluten free" in keywords) or ("gluten-free" in keywords)
+    dairy_free = ("dairy free" in keywords) or ("dairy-free" in keywords) or vegan
+    nut_free = ("nut free" in keywords) or ("nut-free" in keywords)
+    return [gluten_free, dairy_free, vegetarian, vegan, nut_free]
 
 def parse_ingredients(recipe):
     """
