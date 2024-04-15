ea6aa42b74808bce6eddfe004ce99c704fd98f0e
mae97 <13292+mae97@users.noreply.github.coecis.cornell.edu>
2024-04-14 20:27:50 -0400

    Trying 60 dimensions instead of 40 but did not help

--- backend/svd.py
+++ backend/svd.py
@@ -13,20 +13,30 @@ def create_svd_docs_similarity(recipes):
         documents.append({'RecipeId': recipe['RecipeId'], 'Text' : recipe['Text']})
     td_matrix = vectorizer.fit_transform([x['Text'] for x in documents])
     # play around to find best # of dimensions, just using 40 for now
-    words_compressed, _, docs_compressed = svds(td_matrix, k=40)
+    words_compressed, _, docs_compressed = svds(td_matrix, k=60)
     docs_compressed
     docs_compressed = docs_compressed.transpose()
     word_to_index = vectorizer.vocabulary_
     index_to_word = {i:t for t,i in word_to_index.items()}
     words_compressed_normed = normalize(words_compressed, axis = 1)
     docs_compressed_normed = normalize(docs_compressed)
-    return documents, word_to_index, words_compressed_normed, docs_compressed_normed
+    return documents, word_to_index, index_to_word, words_compressed_normed, docs_compressed_normed
 
-def closest_recipes_to_word(documents, word_to_index, words_compressed_normed, docs_compressed_normed, word_in, k = 5):
+def closest_words(documents, word_to_index, index_to_word, words_compressed_normed, docs_compressed_normed, word_in, k = 5):
+    if word_in not in word_to_index: return "Not in vocab."
+    sims = words_compressed_normed.dot(words_compressed_normed[word_to_index[word_in],:])
+    asort = np.argsort(-sims)[:k+1]
+    return [(index_to_word[i],sims[i]) for i in asort[1:]]
+
+def closest_recipes_to_word(documents, word_to_index, index_to_word, words_compressed_normed, docs_compressed_normed, word_in, k = 5):
     if word_in not in word_to_index: return "Not in vocab."
     sims = docs_compressed_normed.dot(words_compressed_normed[word_to_index[word_in],:])
     asort = np.argsort(-sims)[:k+1]
     return [(i, documents[i]['Text'],sims[i]) for i in asort[1:]]
+
+def print_closest_words(words):
+    for word in words:
+        print(word)
 
 def print_closest_recipes(results):
     for recipe in results:
